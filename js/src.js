52d8cea7627e28a1e20e0811d6dbd2b8b3f7dea3
Haley <haley2153@hotmail.com>
2020-12-17 14:52:37 -0800

    Update Website

--- js/src.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let parallax = document.querySelectorAll('.parallax');
-  M.Parallax.init(parallax, {});
-  let accordion = document.querySelectorAll('.collapsible');
-  M.Collapsible.init(accordion, {
-    accordion: false
-  });
-});
